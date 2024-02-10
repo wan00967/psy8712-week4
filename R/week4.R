a36def823c9037e2eea5e63b995c60d4c2750c45
Jessica Wang <wan00967@umn.edu>
2024-02-10 12:19:15 -0600

    changed "qs" naming error

--- R/week4.R
+++ R/week4.R
@@ -3,9 +3,9 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
 # Data Import
-import_tbl <- read_delim("../data/week4.dat", delim = "-", escape_double = FALSE, col_names = c("casenum", "parnum", "stimver", "datadate", "q1 - q5"), trim_ws = TRUE)
+import_tbl <- read_delim("../data/week4.dat", delim = "-", escape_double = FALSE, col_names = c("casenum", "parnum", "stimver", "datadate", "qs"), trim_ws = TRUE)
 glimpse(import_tbl)
-wide_tbl <- separate(import_tbl, "q1 - q5", into = c("q1","q2","q3","q4","q5"), sep = "-")
+wide_tbl <- separate(import_tbl, "qs", into = c("q1","q2","q3","q4","q5"), sep = "-")
 wide_tbl[,5:9]<- sapply(wide_tbl[,5:9], as.integer)
 wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
 wide_tbl[,5:9] <- replace(wide_tbl[,5:9], wide_tbl[,5:9] == 0, NA)
